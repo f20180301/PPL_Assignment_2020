2fd3a3b0ed368d63f4dd633ab6c3af5d8a4903b2
Sahil Kattna <66332768+sahil154@users.noreply.github.com>
2020-10-20 15:36:49 +0530

    Update ppl_tokenise.c

--- ppl_tokenise.c
+++ ppl_tokenise.c
@@ -3,6 +3,10 @@
 #include<stdlib.h>
 #define MAX 100
 
+ char *ha[12];  
+char *op[16][2]; //FOR KEYWORD
+
+
 //type expression Table //fill while TRAVERSING PARSE-TREE
 enum type_var{primitive, rectangular, jagged};
 enum bind_type{static_bind, dynamic_bind, not_applicable};
@@ -34,16 +38,68 @@ token * head=NULL;
 token * tail=NULL;      //Head, Tail of LinkedList for TokenStream
 
 char * getToken(char *lexeme){
-    if(strcmp(lexeme,"list")==0||strcmp(lexeme,"declare")==0){
-        return "Keyword";
+    char c=lexeme[0];
+    int flag=0;
+   /* if(c>=48&&c<=57)
+    {
+        int length=strlen(lexeme);
+         int i=0,fl=0;
+        while(lexeme[i++] != '\0')    
+       {
+        if(lexeme[i] == '.')    // decimal point is present
+        {
+            fl = 1;
+            break;
+        }
+       }
+       if(fl)
+       return "Real";
+       else
+       return "Integer";
+       
     }
-    else if(strcmp(lexeme,"q2")==0){
-        return "Var";
+    else if(strcmp(lexeme,"true")==0||strcmp(lexeme,"false")==0)
+    return "Boolean";*/
+    if((c>=48&&c<=57)||strcmp(lexeme,"true")==0||strcmp(lexeme,"false")==0)
+    return "Primitive Data";
+    else if(flag==0)
+    {
+        for(int i=0;i<12;i++)
+        if(strcmp(lexeme,ha[i])==0)
+          return "Keyword";
+
+          flag=1;
     }
-    else{
-        return "ERROR";
+    if(flag==1)
+    { 
+        for(int i=0;i<16;i++)
+           {
+               if(strcmp(lexeme,op[i][0])==0)
+                 return op[i][1];
+           }
+           flag=2;
     }
-    //DO SILMILARLY
+    if(flag==2)
+    {
+      char d=lexeme[0];
+      int i=1;
+      if(d=='_'||(d>=65&&d<=90)||(d>=97&&d<=122))
+        {
+            while(lexeme[i]!='\0')
+            {
+                d=lexeme[i];
+                if((i<20)&&(d=='_'||(d>=65&&d<=90)||(d>=97&&d<=122)||(d>=48&&d<=57)))
+                {i++;
+                continue;}
+                else
+                return "ERROR";
+                
+            }
+            return "Variable";
+        }
+    }
+   return "Error"; 
+    
 }
 
 token * createNode(char *lexeme,int lineno){      //Return Dynamically Created Record for TOKEN
@@ -70,7 +126,7 @@ void printLinkedList(){         //Test function for printing entire LinkedList o
     token * temp=head;
     printf("\n NOW PRINTING \n\n");
     while(temp!=NULL){
-          printf("%s -> %s --->> %d\n",temp->lexeme,temp->token,temp->lineno);
+          printf("%s         ->       %s             --->>         %d\n",temp->lexeme,temp->token,temp->lineno);
           temp=temp->next;
     }
 }
@@ -104,6 +160,54 @@ void tokeniseSourcecode(char a[100],token *head){       //Function for File Reda
 //driver
 int main(){
     //checkingTokeniser
+ha[0]="program";
+ha[1]="declare";
+ha[2]="list";
+ha[3]="of";
+ha[4]="variables";
+ha[5]="size";
+ha[6]="values";
+ha[7]="integer";
+ha[8]="real";
+ha[9]="boolean";
+ha[10]="array";
+ha[11]="jagged array";
+
+op[0][0]="+";
+op[0][1]="PLUS";
+op[1][0]="-";
+op[1][1]="MINUS";
+op[2][0]="*";
+op[2][1]="MULT";
+op[3][0]="/";
+op[3][1]="DIV";
+op[4][0]="(";
+op[4][1]="RBOP";
+op[5][0]=")";
+op[5][1]="RBCL";
+op[6][0]="{";
+op[6][1]="CBOP";
+op[7][0]="}";
+op[7][1]="CBCL";
+op[8][0]="[";
+op[8][1]="SQOP";
+op[9][0]="]";
+op[9][1]="SQCL";
+op[10][0]="&&&";
+op[10][1]="AND";
+op[11][0]="|||";
+op[11][1]="OR";
+op[12][0]=":";
+op[12][1]="COLON";
+op[13][0]=";";
+op[13][1]="SEMICOLON";
+op[14][0]="..";
+op[14][1]="TWODOT";
+op[15][0]="=";
+op[15][1]="ASSIGN";
+
+
+
     tokeniseSourcecode("filename.txt",head);
     //checking TypeExpression
     typeExpression t;
