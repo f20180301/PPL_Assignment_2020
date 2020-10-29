a23cd64fcbd96e4e0097c34538ccdc4513262e67
Yash Raj Singh <singh.yashraj760@gmail.com>
2020-10-29 19:38:32 +0530

    Added Options

--- driver.c
+++ driver.c
@@ -4,7 +4,6 @@ YASH RAJ SINGH -- 2018A7PS0214P
 SAHIL KATTNA -- 2018A7PS0154P
 AGRAWAL RAJAT RAMESH -- 2018A7PS0182P
 **/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,20 +13,18 @@ AGRAWAL RAJAT RAMESH -- 2018A7PS0182P
 #include "typeExpressionTable.c"
 int main(int argc, char *argv[])
 {
-
+    //INTIALISATION PART
     // 1. Read the grammar.
     grammar *g = malloc(NON_TERMINALS * sizeof(grammar));
     readGrammar("grammar.txt", g);
-
     // 2. Tokenize the source code
     if (argc == 2)
         tokeniseSourcecode(argv[1], head);
     else
     {
-        printf("Please provide the sourcefile.txt as commaand line\n");
+        printf("Please provide the sourcefile.txt as command line\n");
         return 0;
     }
-
     // 3. Create the parseTree
     //create the start node
     posToken = head;
@@ -38,21 +35,75 @@ int main(int argc, char *argv[])
     p->parent = p;
     p->sibling = NULL;
     p->firstChild = NULL;
-	p->exp_type.tag = not_app;
-	p->depth = 0;
-	
-    createParseTree(p, g);
+    p->depth = 0;
+    p->exp_type.tag = not_app;
+    // if (posToken != NULL)
+    // {
+    //     printf("ERROR READING INPUT TESTCASE (IT OCCURS USUALLY IF AN UNKNOWN SYMBOLS APPEAR WHICH CANNOT DOEAS NOT MATCH ANY RULE)\n");
+    //     return 0;
+    // }
+    int n;
+    // //4. Printing the parse tree
+    // printParseTree(p);
 
-    if (posToken != NULL)
+    // traverse_parse_tree(p);
+    // printParseTree(p);
+    // printTypeExpressionsTable(table, "typeExpressionTable.txt");
+    do
     {
-        printf("ERROR READING INPUT TESTCASE (IT OCCURS USUALLY IF AN UNKNOWN SYMBOLS APPEAR WHICH CANNOT DOEAS NOT MATCH ANY RULE)\n");
-        return 0;
-    }
-    //4. Printing the parse tree
-    printParseTree(p);
-
-    traverse_parse_tree(p);
-    printParseTree(p);
-    printTypeExpressionsTable(table, "typeExpressionTable.txt");
+        printf("Please Choose your option : \n");
+        scanf("%d", &n);
+        switch (n)
+        {
+        case 0:
+            printf("EXITING!!\n");
+            break;
+        case 1:
+            createParseTree(p, g);
+            if (posToken != NULL)
+            {
+                printf("ERROR READING INPUT TESTCASE (IT OCCURS USUALLY IF AN UNKNOWN SYMBOLS APPEAR WHICH CANNOT DOEAS NOT MATCH ANY RULE)\n");
+                return 0;
+            }
+            printf("Parse Tree Created Successfully\n");
+            break;
+        case 2:
+            createParseTree(p, g);
+            if (posToken != NULL)
+            {
+                printf("ERROR READING INPUT TESTCASE (IT OCCURS USUALLY IF AN UNKNOWN SYMBOLS APPEAR WHICH CANNOT DOEAS NOT MATCH ANY RULE)\n");
+                return 0;
+            }
+            traverse_parse_tree(p);
+            printf("Parse Tree Traversal Successfull. Errors if any are Printed\n");
+            break;
+        case 3:
+            createParseTree(p, g);
+            if (posToken != NULL)
+            {
+                printf("ERROR READING INPUT TESTCASE (IT OCCURS USUALLY IF AN UNKNOWN SYMBOLS APPEAR WHICH CANNOT DOEAS NOT MATCH ANY RULE)\n");
+                return 0;
+            }
+            traverse_parse_tree(p);
+            printParseTree(p);
+            printf("Parse Tree Printing in parseTree.txt and Traversal Successfull. Errors if any are Printed\n");
+            break;
+        case 4:
+            createParseTree(p, g);
+            if (posToken != NULL)
+            {
+                printf("ERROR READING INPUT TESTCASE (IT OCCURS USUALLY IF AN UNKNOWN SYMBOLS APPEAR WHICH CANNOT DOEAS NOT MATCH ANY RULE)\n");
+                return 0;
+            }
+            traverse_parse_tree(p);
+            printParseTree(p);
+            printTypeExpressionsTable(table, "typeExpressionTable.txt");
+            printf("Parse Tree Printing in parseTree.txt, Traversal and Type Expression Table Printing in typeExpressionTable.txt Successfull. Errors if any are Printed\n");
+            break;
+        default:
+            printf("Sorry! Enter a number between 0-4\n");
+            break;
+        }
+    } while (n != 0);
     return 0;
 }
